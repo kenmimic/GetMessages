8ed6352940f433f6a0a4561f3dd291e1389817e1
root <root@localhost.localdomain>
2020-02-13 09:44:33 -0800

    First commit and initial script

---
+++ getMessage.py
@@ -0,0 +1,21 @@
+import sys,os
+import requests
+import json
+import urllib2#,urllib2,urllib3,re
+
+url = 'https://dhl2-fw.gbcluster.net/'
+
+#with open('/usr/share/secLists/Discovery/Web-Content/common.txt','r') as folders:
+with open(sys.argv[1],'r') as folders:
+  for folder in folders.readlines():
+    enumerate_url = url+folder
+    response = os.popen('curl -i %s' %(enumerate_url)).read()
+    
+    if "Token" in response:
+	    with open('./Missing_Token_path','a') as outfile:
+	        outfile.write(enumerate_url)
+	        #outfile.write(response)
+	        print enumerate_url
+	        print response
+	        outfile.close()
+folders.close()
